9685bd1db2857084e6af348066a36ce9e333d618
Shrivats Sriram <shrivats65a@gmail.com>
2025-02-22 23:49:28 -0600

    temp

--- backend/similar.py
+++ backend/similar.py
@@ -32,7 +32,7 @@ class similar_words():
 
     # what we do is we load the data frame in the same way we did the 
     def get_data(self):
-            folder_path = "../dreams"
+            folder_path = "./dreams"
             count = 0
             all_files = []
             for i in os.listdir(folder_path):
@@ -111,27 +111,6 @@ if __name__ == "__main__":
 
 
 # what we do is we load the data frame in the same way we did the 
-def get_data(self):
-        folder_path = "./dreams"
-        count = 0
-        all_files = []
-        for i in os.listdir(folder_path):
-            if i.endswith('.json'):
-                count+=1
-                all_files.append(i)
-        dreams = []
-        for i in all_files:
-            full_name  = os.path.join(folder_path,i)
-            with open(full_name,'r') as f:
-                data = json.load(f)
-                for x in data['dreams']:
-                    cleaned_content = clean_text(x.get("content", ""))
-                    dreams.append({
-                        "content": cleaned_content
-                    })
-                    
-        df = pd.DataFrame(dreams)
-        self.data_frame = df
 #the  we get a score for each of the dreams so use tf-idf
 #add the scores into the dataframe
 #then we can clean the data in a few steps
